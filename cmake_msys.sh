773927e7189f641b6e4ce64ac2ac8ce171c52aaf
chenhu84 <ht.chen@mail.utoronto.ca>
2020-05-23 23:02:42 -0400

    ?

--- cmake_msys.sh
+++ cmake_msys.sh
@@ -1,3 +1,4 @@
+mkdir build
 cd build
 cmake .. -G "Unix Makefiles"
 make 
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
-#include "Hello.h"
+#include <iostream>
+#include <Scanner.h>
 
 int main(int argc, char *argv[])
 {
-    Hello hi;
-    hi.print();
+    
+    Token t(TOKEN_BANG, "!", 100);
+    std::cout <<  t.lexeme << " " << t.line << std::endl;
     return 0;
 }
