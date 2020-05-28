ea488fe541fce2c74c92c1cc16368658e46ad4ae
chenhu84 <ht.chen@mail.utoronto.ca>
2020-05-27 21:42:03 -0400

    parse ast calc.lox, need vm

--- src/AstPrinter.cpp
+++ src/AstPrinter.cpp
@@ -2,30 +2,33 @@
 #include <iostream>
 #include "Expr.h"
 #include "AstPrinter.h"
-
-
+#include <string>
 
 void AstPrinter::printAst(Expr &expr) {
     expr.accept(*this);
 }
 
 void AstPrinter::visitLiteralExpr(const LiteralExpr &expr) {
-    std::cout << expr.token.lexeme << " ";
+    std::cout << "( " << expr.token.lexeme << " )";
 }
 
 void AstPrinter::visitUnaryExpr(const UnaryExpr &expr) {
+    std::cout << "( ";
     std::cout << expr.opr.lexeme;
     expr.right->accept(*this);
+    std::cout << " )";
 }
 
 void AstPrinter::visitGroupingExpr(const GroupingExpr &expr) {
-    std::cout << "(";
+    std::cout << "( grouping ";
     expr.innerExpr->accept(*this);
     std::cout << ")";
 }
 
 void AstPrinter::visitBinaryExpr(const BinaryExpr &expr) {
+    std::cout << "( ";
     std::cout << expr.opr.lexeme;
     expr.left->accept(*this);
     expr.right->accept(*this);
+    std::cout << " )";
 }
